224671f4222e16a905059a1ae6bec09770aea20f
Vinicius Araujo <viniciusaraujoaj@gmail.com>
2024-04-24 17:58:40 -0300

    javascript concluido

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const btn = document.querySelector(".menu");
+const links = document.querySelector(".links");
 
+btn.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
